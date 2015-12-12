4b343ab8bb257f751d19e24bc8fab8e11d70a319
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2015-12-12 16:26:06 -0600

    Adding more people

--- www/app/people/people.service.js
+++ www/app/people/people.service.js
@@ -14,16 +14,58 @@
 	var peopleData = [
 		{
 			email: 'bbarnett@rightpoint.com',
+			location: 'Chicago',
 			name: 'Brandon Barnett',
 			phone: '8475089028',
 			practice: 'Application Development',
 			username: 'bbarnett'
 		},
 		{
+			email: 'apetersen@rightpoint.com',
+			location: 'Chicago',
+			phone: null,
+			name: 'Anne Petersen',
+			practice: 'User Experience',
+			username: 'apetersen'
+		},
+		{
 			email: 'ssartell@rightpoint.com',
+			location: 'Chicago',
 			phone: null,
 			name: 'Sean Sartell',
 			practice: 'Application Development',
 			username: 'ssartell'
+		},
+		{
+			email: 'jsears@rightpoint.com',
+			location: 'Chicago',
+			phone: null,
+			name: 'Jason Sears',
+			practice: 'Application Development',
+			username: 'jsears'
+		},
+		{
+			email: 'stonkin@rightpoint.com',
+			location: 'Denver',
+			phone: null,
+			name: 'Steve Tonkin',
+			practice: 'Application Development',
+			username: 'stonkin'
+		},
+		{
+			email: 'sumber@rightpoint.com',
+			location: 'Chicago',
+			phone: null,
+			name: 'Sarah Umber',
+			practice: 'Delivery Support',
+			username: 'sumber'
+		},
+		{
+			email: 'vvaisnys@rightpoint.com',
+			location: 'Chicago',
+			phone: null,
+			name: 'Vaiva Vaisnys',
+			practice: 'Delivery Support',
+			username: 'vvaisnys'
 		}];
 } ());
